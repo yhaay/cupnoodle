09a22ad4391ecb2e03045444f7108bbb8e75be6e
yhaay <yyhaa123@gmail.com>
2015-03-02 03:15:37 +0900

    test

--- application/views/main_cupnoodle.php
+++ application/views/main_cupnoodle.php
@@ -2,14 +2,14 @@
       <!-- Example row of columns -->
       <div class="row">
       <?php 
-      	foreach($cupnoodle as $row)
+      	foreach($cupnoodle as $row) {
       ?>
         <div class="col-md-4">
           <h2><?php $row->name?></h2>
           <p><?php $row->rate?></p>
           <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div>
-      
+      }
       <hr>
 
     </div>
