8d9e7abc159fe5e0caaba41f22d766dca7eedcc4
yhaay <yyhaa123@gmail.com>
2015-03-02 04:00:58 +0900

    aerasre

--- application/views/head.php
+++ application/views/head.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+<meta http-equiv="content-type" content="text/html;charset=utf8"/> 
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="/static/lib/bootstrap/css/bootstrap.min.css"
